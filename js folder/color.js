9d90f7569f7a26bb6648f0004af8a61636f5d1be
Robin4533704 <robinhossen8428@gmail.com>
2025-03-03 12:36:01 +0600

    fast commit

--- js folder/color.js
+++ js folder/color.js
@@ -1,22 +1,23 @@
- const colors = [
-     'lightblue', 'lightgreen','lightcoral', 'lightgoldenrodyellow', 'lightpink', 'lightskyblue'];
+const colors = ['lightblue', 'lightgreen', 'lightcoral', 'lightgoldenrodyellow', 'lightpink', 'lightskyblue']; 
+let currentColorIndex = 0;
 
-   let currentColorIndex  = 0;
-document.getElementById('colorButton').addEventListener('click', function (){
-  document.body.style.backgroundColor = colors[currentColorIndex];
-  currentColorIndex++;
-  if(currentColorIndex>=colors.length){
-    currentColorIndex=0;
-  }
-  this.style.backgroundColor = colors[currentColorIndex];
 
-})
+document.getElementById('colorButton').addEventListener('click', function () {
 
-  // button add
-  document.getElementById('Discover-box').addEventListener('click', function(){
-window.location.href = "./secend.html";
+    document.body.style.backgroundColor = colors[currentColorIndex];
+      currentColorIndex++;
+    
+    if (currentColorIndex >= colors.length) {
+        currentColorIndex = 0;
+    }
+    
+    this.style.backgroundColor = colors[currentColorIndex];
+});
 
-  })
-  document.getElementById('back-btn').addEventListener('click', function(){
-    window.location.hre= "./index.html"
-  })
+document.getElementById('Discover-box').addEventListener('click', function() {
+    window.location.href = "./secend.html";
+});
+
+document.getElementById('back-btn').addEventListener('click', function() {
+    window.location.href = "./index.html"; 
+});
